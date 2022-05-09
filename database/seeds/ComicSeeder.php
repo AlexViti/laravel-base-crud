451f832c288d97235ba536ada87f9162f2a3261c
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-05-09 14:20:00 +0200

    Add migration and seeder

---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+
+class ComicSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $comics = config('comics');
+    foreach ($comics as $comic) {
+      $comic = new Comic($comic);
+      $comic->save();
+    }
+  }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,12 @@
 @extends('layouts.templates.base')
+
+@section('title', 'Home')
+
+@section('content')
+
+  <main class="container">
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+  </main>
+
+@endsection
